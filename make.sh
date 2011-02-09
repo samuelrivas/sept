0f8112649c1639501e065e3320dbbb27f8a3ff27
Samuel Rivas <samuelrivas@gmail.com>
2011-02-09 13:42:35 +0100

    FUNCT added the option to pass an argument to make.sh to compile only one directory

--- make.sh
+++ make.sh
@@ -11,7 +11,13 @@ for i in `find $HOME -name ebin`; do
     APPS="${APPS} -pa $i";
 done
 
-for i in `find $HOME -name 'src'`; do
+if [ -z "$1" ]; then
+    DIRS=`find $HOME -name 'src'`;
+else
+    DIRS="$1/src";
+fi
+
+for i in $DIRS; do
     cd $i
     echo "Making `dirname $i | xargs basename`"
     erl -eval 'case make:all([debug_info, {outdir, "../ebin"}]) of error -> halt(1); _ -> halt(0) end.' -noshell ${APPS}
