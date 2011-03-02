f20e96816affb6ff966874e044eb5ba0057be4f6
Samuel Rivas <samuelrivas@gmail.com>
2011-03-02 12:37:48 +0100

    FUNCT use ERL_LIBS instead of a ton of -pa
    Grossly simplify how we start erlang

--- run.sh
+++ run.sh
@@ -3,9 +3,4 @@
 # Start a shell with the code path set to load all erlang apps in the project
 
 ROOT=$PWD/`dirname $0`
-APPS=
-for i in `find ${ROOT} -name ebin`; do
-    APPS="${APPS} -pa $i";
-done
-
-erl ${APPS} $@
+ERL_LIBS=$ROOT:$ERL_LIBS erl $@
