0c35385f15613f4e1c6c79d1b6b3b526d2e76499
Samuel Rivas <samuel.rivas@interoud.com>
2011-11-04 13:23:54 +0100

    BUGFIX make run.sh run with absolute paths also

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 #
 # Start a shell with the code path set to load all erlang apps in the project
 
-ROOT=$PWD/`dirname $0`
+ROOT="$( cd "$( dirname "$0" )" && pwd )"
+
 ERL_LIBS=$ROOT:$ERL_LIBS erl $@
